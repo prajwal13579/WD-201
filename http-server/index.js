0307ca2b8ed1443c2b46a177f3bae94df97d7157
prajwal13579 <99010077+prajwal13579@users.noreply.github.com>
2023-02-28 22:07:52 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,46 +1,54 @@
-let http = require("http");
-let fs = require("fs");
+const http = require("http");
+const fs = require("fs");
+const args = require('minimist')(process.argv.slice(2));
 
-let home = '';
-let projects = '';
-let registration = '';
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
 
-fs.readFile('home.html', (err,data)=>{
-    if(err) throw err; 
-    home = data.toString();
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
 });
 
-
-fs.readFile('projects.html', (err,data)=>{
-    if(err) throw err; 
-    home = data.toString();
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
 });
 
-fs.readFile('registration.html', (err,data)=>{
-    if(err) throw err; 
-    home = data.toString();
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
 });
 
-http.createServer((request, response)=>{
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  http
+  .createServer((request, response) => {
     let url = request.url;
-
-    switch(url){
-        case '/project': 
-            response.writeHead(200, { "Content-Type": "text/html" });
-            response.write(home);
-            response.write(projects);
-            response.end();
-            break;
-        case '/registration':
-            response.writeHead(200, { "Content-Type": "text/html" });
-            response.write(home);
-            response.write(registration);
-            response.end();
-            break; 
-        default:
-            response.writeHead(200, { "Content-Type": "text/html" });
-            response.write(home);
-            response.write(home);
-            response.end();
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
     }
-}).listen(5000);
+  })
+  .listen(args.port);
+});
