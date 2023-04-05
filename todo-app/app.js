728912f895aca6c937f9a60c6a16f4115e3c23e5
prajwal13579 <99010077+prajwal13579@users.noreply.github.com>
2023-04-05 18:28:34 +0530

    updated

--- todo-app/app.js
+++ todo-app/app.js
@@ -4,11 +4,27 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
+
 app.use(bodyParser.json());
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
+app.set("view engine", "ejs");
+
+app.get("/", async (request,response) =>{
+  const allTodos = await Todo.getTodos();
+  if (request.accepts("html")){
+    response.render("index", {
+      allTodos,
+    });
+  }
+  else{
+    response.json({
+      allTodos
+    });
+  }
 });
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
@@ -36,13 +52,17 @@ app.get("/todos/:id", async function (request, response) {
   }
 });
 
-app.post("/todos", async function (request, response) {
+app.post("/todos", async function (request, response)  {
   try {
-    const todo = await Todo.addTodo(request.body);
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+    });
+
     return response.json(todo);
   } catch (error) {
     console.log(error);
-    return response.status(422).json(error);
+    return response.status(422).json(error); 
   }
 });
 
